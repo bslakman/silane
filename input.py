ef9771f8344b645c1f287abdc05e54dbe337e2dd
Belinda Slakman <slakman.b@husky.neu.edu>
2016-10-24 12:48:06 -0400

    Removed pdep for now.

--- input.py
+++ input.py
@@ -144,14 +144,14 @@ model(
     maximumEdgeSpecies=100000
 )
 
-pressureDependence(
-    method='modified strong collision',
-    maximumGrainSize=(0.5,'kcal/mol'),
-    minimumNumberOfGrains=250,
-    temperatures=(300,2000,'K',8),
-    pressures=(0.01,20,'bar',5),
-    interpolation=('Chebyshev', 6, 4),
-)
+#pressureDependence(
+#    method='modified strong collision',
+#    maximumGrainSize=(0.5,'kcal/mol'),
+#    minimumNumberOfGrains=250,
+#    temperatures=(300,2000,'K',8),
+#    pressures=(0.01,20,'bar',5),
+#    interpolation=('Chebyshev', 6, 4),
+#)
 
 options(
     units='si',
